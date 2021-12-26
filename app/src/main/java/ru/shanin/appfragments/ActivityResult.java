3e97b907d19b47152d1588454a5236104c44401e
redhatdeath <redhatdeath@yandex.ru>
2021-12-26 16:41:52 +0500

    add fragment in main activity sensor and LiveData works

--- app/src/main/java/ru/shanin/appfragments/ActivityResult.java
+++ app/src/main/java/ru/shanin/appfragments/ActivityResult.java
@@ -1,16 +1,12 @@
 package ru.shanin.appfragments;
 
-import android.content.ComponentName;
 import android.content.Context;
 import android.content.Intent;
-import android.content.ServiceConnection;
 import android.os.Bundle;
-import android.os.IBinder;
 import android.util.Log;
 
 import androidx.appcompat.app.AppCompatActivity;
-
-import ru.shanin.appfragments.service.TypeLightService;
+import androidx.fragment.app.Fragment;
 
 public class ActivityResult extends AppCompatActivity {
 
@@ -61,12 +57,23 @@ public class ActivityResult extends AppCompatActivity {
      */
 
     private void launchFragmentResult(String data) {
+        Fragment fragment;
+//        if(bla bla bla){}
+//        else {}
+        fragment = FragmentResult.newInstanceWithInputData(data);
+//        getSupportFragmentManager()
+//                .popBackStack();
         getSupportFragmentManager()
                 .beginTransaction()
-                .replace(
+//                .addToBackStack("null")
+//                .replace(
+//                        R.id.fragmentActivityResult,
+//                        FragmentResult.newInstanceWithInputData(data)
+//                )
+                .add(
                         R.id.fragmentActivityResult,
-                        FragmentResult.newInstanceWithInputData(data)
+                        fragment
                 )
-                .commitNow();
+                .commit();
     }
 }
